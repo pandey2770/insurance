f8aab49e0f58e9f8effc2369c728c16a99f4da32
pandey2770 <pandey.abhishek2770@gmail.com>
2019-12-24 11:18:12 +0530

    uploadPage

--- db.sql
+++ db.sql
@@ -13,4 +13,25 @@ CREATE TABLE usertable (
   password varchar,
   user_name varchar UNIQUE
 );
+
 insert into usertable values ('40da9106-2d21-4ec3-bc40-38a9388c7e4d','$2b$10$SxWTap9pvqtzLg3inpGzVOax/v3S8nS0CX9xN3SNE8LLjdJe97xEu','ssdn');
+
+CREATE TABLE stateMaster (  
+  id uuid PRIMARY KEY,
+  stateCode varchar,
+  stateName varchar
+);
+
+CREATE TABLE cityMaster (  
+  id uuid PRIMARY KEY,
+  cityCode varchar,
+  cityName varchar,
+  stateCode varchar
+);
+
+CREATE TABLE pinCodeMaster (  
+  id uuid PRIMARY KEY,
+  stateName varchar,
+  cityName varchar,
+  pinCode varchar
+);
